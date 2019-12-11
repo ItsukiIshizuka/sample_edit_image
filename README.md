61a886bafccf735bfad33115d1e8dc58beb1df79
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-12-11 16:58:14 +0900

    image create

--- README.md
+++ README.md
@@ -4,11 +4,23 @@
 * pry-rails
 * jquery-rail https://github.com/rails/jquery-rails
 * haml-rails https://github.com/haml/haml-rails
+* carrierwave 
 
 ## 構築手順
 * productテーブル作成(name:string)
 * imageテーブル作成(name:string, product_id:外部キー)
 * 親 productモデル 子 imageモデル で構築
+* "carrierwave"導入後、以下のコードを実行
+
+```
+rails g uploader images
+```
+* image モデルに以下の一文を追加
+
+```
+mount_uploader :name, ImagesUploader
+```
+* 画像のアップロードテスト
 
 ## What
 * fields_forを用いた画像複数モデルへの登録
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,7 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
+    @product.images.build
   end
 
   # GET /products/1/edit
@@ -24,6 +25,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
+    
     @product = Product.new(product_params)
 
     respond_to do |format|
@@ -35,6 +37,7 @@ class ProductsController < ApplicationController
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # PATCH/PUT /products/1
@@ -69,6 +72,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name)
+      params.require(:product).permit(:name, images_attributes: [:image])
     end
 end
